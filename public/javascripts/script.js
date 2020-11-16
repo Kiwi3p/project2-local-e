614e5aaa64d39a01736eaa390254d60d1518d62a
John Transue <johnktransue@gmail.com>
2020-11-16 17:31:43 +0000

    small changes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -21,12 +21,13 @@ x.innerHTML = "Latitude: " + position.coords.latitude +
 "<br>Longitude: " + position.coords.longitude;
 }
 
+
 let map;
 function initMap() {
   const lisbon = { lat: 38.7117206, lng: -9.1264315 };
    map = new google.maps.Map(document.getElementById("map"), {
     zoom: 17,
     center: lisbon
-  })
+  });
 }
 
