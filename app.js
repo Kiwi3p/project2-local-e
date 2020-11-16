74b62999987da075c5af603428e4b02d876791ac
AliLop”git config --global user.email alicia.hl87@gmail.comgit config --global user.name AliLop” <65405578+AliLop@users.noreply.github.com>
2020-11-16 16:47:07 +0000

    Express - Generated with IronGenerator

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Local-E';
 app.locals.subtitle = 'Keep it Local';
-
+app.locals.gmapKey = process.env.GOOGLE_MAPS_API_KEY;
 
 const index = require('./routes/index');
 app.use('/', index);
@@ -73,9 +73,6 @@ app.use('/', auth);
 const yelp = require('./routes/yelp');
 app.use('/', yelp);
 
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-app.locals.gmapKey = process.env.GOOGLE_MAPS_API_KEY;
 
 
 module.exports = app;
