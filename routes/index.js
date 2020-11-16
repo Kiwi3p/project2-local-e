5d512120d1b8e3d227b959d74125f18ca81fd28d
John Transue <johnktransue@gmail.com>
2020-11-16 12:07:58 +0000

    messy yelp API added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const yelp = require('yelp-fusion');
 
 function requireLogin(req, res, next) {
   if(req.session.currentUser) {
@@ -18,12 +19,33 @@ function requireLogin(req, res, next) {
       //User in the session allows the time set for teh session!
     });
 
-    //profile
+    //profile... yelp
+
+    
+
+    
     router.get('/profile', requireLogin, (req, res) => {
-      currentLocation = req.session.currentUser.location;
-      res.render('private/profile');
+      const apiKey = 'WgRag4zBmo7OTNmEPv_H4JsjK1OhtfSYeCIG99_4-xK7R0Ref9OP0RlVWb_5GssPYF9KucWPXl70qYDIzIGruWthGl3ZgcC9YuxBYOg8T21PmTk6kzRKSTf2InKtX3Yx';
+      const client = yelp.client(apiKey);
+      const storedLocation = req.session.currentUser.location;
+      const searchRequest = {
+      term:'Farmers market',
+      location: storedLocation,
+      //latitude: 38.7600325,   //input {{LAT}} data here
+      //longitude: -9.1400807, //input {{LONG}} data here
+      radius: 5000, 
+      limit: 8
+    };
+      client.search(searchRequest).then(response => {
+        const firstResult = response.jsonBody.businesses;
+      //  const prettyJson = JSON.stringify(firstResult, null, 4);
+        console.log(firstResult);
+      res.render('private/profile', {print: firstResult});
       console.log(currentLocation);
+    }).catch(e => {
+      console.log(e);
     });
+  });
 
     //map
     router.get('/map', requireLogin, (req, res) => {
---
+++ routes/yelp.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const yelp = require('yelp-fusion');
+const express = require('express');
+const hbs = require('hbs');
+
+// Place holder for Yelp Fusion's API Key. Grab them
+// from https://www.yelp.com/developers/v3/manage_app
+const apiKey = 'WgRag4zBmo7OTNmEPv_H4JsjK1OhtfSYeCIG99_4-xK7R0Ref9OP0RlVWb_5GssPYF9KucWPXl70qYDIzIGruWthGl3ZgcC9YuxBYOg8T21PmTk6kzRKSTf2InKtX3Yx';
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+const searchRequest = {
+  term:'Farmers market',
+  location: 'Lisbon, pt',
+  //latitude: 38.7600325,   //input {{LAT}} data here
+  //longitude: -9.1400807, //input {{LONG}} data here
+  radius: 5000, 
+  limit: 8
+};
+
+const searchDeny = {
+  term: 'pingo doce',
+  latitude: 38.7600325,
+  longitude: -9.1400807,
+  radius: 5000,
+  limit: 8
+}
+
+/*
+let listOfSupermarketsFromAPI = [];
+
+let filteredSupermarkets = listOfSupermarketsFromAPI.filter((supermarket) => {
+  return supermarket.name !== 'pingo doce' || 
+}) */
+
+
+const client = yelp.client(apiKey);
+
+
+app.get('/profile', (req, res) =>  {
+client.search(searchRequest).then(response => {
+  const firstResult = response.jsonBody.businesses;
+//  const prettyJson = JSON.stringify(firstResult, null, 4);
+  console.log(firstResult);
+  res.render('/private/profile', {print: firstResult})
+}).catch(e => {
+  console.log(e);
+});
+});
+
+app.listen(3000, () => console.log('My Yelp project running on port 3000 🎧 🥁 🎸 🔊'));
